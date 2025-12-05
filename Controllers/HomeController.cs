d78f7fb6ff42b4664ffd03f58ab8f32ede9170e7
MindteckSinghPankaj <pankaj.kumarsinghjr@philips.com>
2025-12-05 15:25:32 +0530

    Updated HomeController.cs and Indexs.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,14 +7,26 @@ namespace AzureWebApp.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
     {
+        string appName = _configuration["AppSettings:ApplicationName"];
+        bool paymentEnabled = _configuration.GetValue<bool>("AppSettings:EnablePayment");
+        int maxAttempts = _configuration.GetValue<int>("AppSettings:MaxLoginAttempts");
+        string email = _configuration["AppSettings:SupportEmail"];
+
+        ViewBag.AppName = appName;
+        ViewBag.Payment = paymentEnabled;
+        ViewBag.MaxAttempts = maxAttempts;
+        ViewBag.Email = email;
+        
         return View();
     }
 
